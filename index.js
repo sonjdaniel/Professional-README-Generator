8998b0414b764c2ca3964057514029c668c24fc2
sonjdaniel <sonjdaniel@gmail.com>
2023-05-07 10:40:05 -0500

    created a template of the question, Need to start an array of question

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
+// Create an array of questions for user input. Template for the questions.
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter a title");
+        return false;
+      }
+    },
+  },
+];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
